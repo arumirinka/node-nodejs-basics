d4a81ac6248947f933cb7f1acad2c67e0a4f49bc
Irina Stepanova <arumirinka@github.com>
2025-04-29 02:44:37 +0600

    feat: add solution for fs tasks

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { join } from 'node:path';
+import { readFile } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToRead.txt');
+    
+    readFile(filePath, 'utf-8', (err, data) => {
+        if (err) throw new Error('FS operation failed');
+
+        console.log(data);
+    });
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import { join } from 'node:path';
+import { access, constants, rename as renameFs } from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const wrongPath = join(__dirname, 'files', 'wrongFilename.txt');
+    const properPath = join(__dirname, 'files', 'properFilename.md');
+
+    access(properPath, constants.F_OK, (err) => {
+        if (err) {
+            renameFs(wrongPath, properPath, (err) => {
+                if (err) throw new Error('FS operation failed');
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await rename();
