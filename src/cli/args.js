a50e25dc7893d09df3e63416d84abf72b3708d89
Irina Stepanova <arumirinka@github.com>
2025-04-29 03:06:08 +0600

    feat: add solution for cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    const argsArr = [];
+    args.forEach((arg, i) => {
+        if (arg.startsWith('--')) {
+            argsArr.push(`${arg.slice(2)} is ${args[i+1]}`);
+        }
+    });
+    console.log(argsArr.join(', '));
 };
 
 parseArgs();
