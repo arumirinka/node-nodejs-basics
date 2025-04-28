5e8e75f84145dc474292e6ba8c4653caa285a7ae
Irina Stepanova <arumirinka@github.com>
2025-04-29 05:56:25 +0600

    feat: add solution for zip compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompress = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+    const resFile = join(import.meta.dirname, 'files', 'archive.gz');
+    const gZip = createGzip();
+    const src = createReadStream(fileToCompress);
+    const dest = createWriteStream(resFile);
+
+    pipeline(src, gZip, dest, (err) => {if (err) console.log(err)});
 };
 
 await compress();
