0118e5fb236a55dc7607af53e385109d09351e02
Irina Stepanova <arumirinka@github.com>
2025-04-29 05:49:44 +0600

    feat: add solution for cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { join } from 'node:path';
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const file = join(import.meta.dirname, 'files', 'script.js');
+    const childProcess = spawn('node', [file, ...args], {stdio: [process.stdin, process.stdout, 'pipe'] });
+
+    childProcess.on('error', (error) => {
+        console.error(`Error: ${error}`);
+    });
+
+    childProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
