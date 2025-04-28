a90cd2f7dbacb1b20df16163517a4d605f7d637e
Irina Stepanova <arumirinka@github.com>
2025-04-29 04:12:27 +0600

    feat: add solution for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const readStream = createReadStream(file);
+    readStream.on('readable', () => {
+        const data = readStream.read();
+        if (data)
+          hash.update(data);
+        else {
+          console.log(hash.digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
